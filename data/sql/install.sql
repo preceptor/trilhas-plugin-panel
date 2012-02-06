369a5f08fe0e4580fa68378c96d861a09a71d464
Renerson Ian <renerson.i@gmail.com>
2012-02-06 13:51:20 -0200

    add panel_note

--- data/sql/install.sql
+++ data/sql/install.sql
@@ -8,5 +8,19 @@ CREATE TABLE IF NOT EXISTS `panel` (
   KEY `item_id` (`item_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
-AALTER TABLE `panel`
+CREATE TABLE IF NOT EXISTS `panel_note` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `panel_id` bigint(20) DEFAULT NULL,
+  `note` tinyint(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `panel_id` (`panel_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=72 ;
+
+ALTER TABLE `panel`
   ADD CONSTRAINT `panel_ibfk_1` FOREIGN KEY (`classroom_id`) REFERENCES `classroom` (`id`);
+
+ALTER TABLE `panel_note`
+  ADD CONSTRAINT `panel_note_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `panel_note_ibfk_2` FOREIGN KEY (`panel_id`) REFERENCES `panel` (`id`);
